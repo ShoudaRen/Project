19c2fff9a51b57482e13146c22abd6e98b5a92b7
Shouda Ren <2521509R@student.gla.ac.uk>
2021-11-03 14:13:07 +0000

    project establish

---
+++ myProject/src/test/java/com/cy/myProject/MyProjectApplicationTests.java
@@ -0,0 +1,24 @@
+package com.cy.myProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+	@Autowired
+  private DataSource dataSource;
+	@Test
+	void contextLoads() {
+	}
+
+	//TEST DATABASE
+	@Test
+	void  getConnection() throws SQLException {
+		System.out.println(dataSource.getConnection());
+	}
+
+}
